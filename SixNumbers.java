1e325770ea2c753b108688162f180a930076e3ba
frimps-astro <frimpsastro@gmail.com>
2021-10-01 17:16:35 +0000

    Java Day 1: Labwork 1 Question 6 part A:updated

--- SixNumbers.java
+++ SixNumbers.java
@@ -5,6 +5,8 @@ public class SixNumbers{
 		//print 6 numbers from range 1 to 49
 		Random rand = new Random();
 		for(int i=0; i<6; i++){
+			//+1 is to make sure if 0 is generated it increases to 1 to meet
+			//the range requirement; it would be generated up to 48(+1)->49;
 			System.out.println(rand.nextInt(49)+1);
 		}
 	}
