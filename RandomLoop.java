94be74dbdc9a02d0c89af4b0be4d61f0c8a610d7
frimps-astro <frimpsastro@gmail.com>
2021-10-01 16:47:39 +0000

    Bug Fixes** Java Day 1: Labwork 1 Question part A; do while loop

--- RandomLoop.java
+++ RandomLoop.java
@@ -8,9 +8,9 @@ public class RandomLoop{
 		int num = 0;
 		do{
 			//assuming the range specified(-3 to +3) is inclusive
-			//java by default doesn't include the bound so making 4 to include 3
+			//java by default doesn't include the bound so I'm using 7(^6(-3)) to include 3
 			//then random number -3 to include -3
-			num = rand.nextInt(4)-3;
+			num = rand.nextInt(7)-3;
 			System.out.println(num);
 		}while(num != 0);
 	}
