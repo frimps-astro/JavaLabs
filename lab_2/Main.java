1b4982cc92cd92190fcd1c794597ff8483162828
frimps-astro <frimpsastro@gmail.com>
2021-10-04 16:08:25 +0000

    First Question on instantiating an array

---
+++ lab_2/Main.java
@@ -0,0 +1,24 @@
+class ArrayClass{
+
+	public ArrayClass(int size){
+		int[] array = this.setArray(size);
+
+		for(int i:array)
+			System.out.println(i);
+	}	
+	public int[] setArray(int size){
+		int[] array = new int[size];
+		for(int i=0; i<size; i++){
+			array[i] = (i+1);
+		}
+
+		return array;
+	}
+}
+
+public class Main{
+
+	public static void main(String args[]){
+		ArrayClass arr = new ArrayClass(10);	
+	}
+}
