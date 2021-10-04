6de72b16cc458cf64a43421505a2adbcb5b7c6e6
frimps-astro <frimpsastro@gmail.com>
2021-10-04 15:54:18 +0000

    Java Packaging and Deployment labs

--- Hello.java
+++ Hello.java
@@ -1,7 +1,12 @@
+//package course;
+
+import course.demo.AnotherClass;
+
 public class Hello{
 	public static void main(String args[]){
 		String text = "Hello World!";
 
 		System.out.println(text);
+		AnotherClass.display();
 	}
 }
